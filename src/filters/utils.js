b6001081e10c52faffd610ff128cc0aea0b0af92
Erica Mitchell <mitchell.erica31@gmail.com>
2019-11-25 11:59:57 -0500

    propose change to require statements

--- src/filters/utils.js
+++ src/filters/utils.js
@@ -26,6 +26,7 @@ const slugify = (text) =>
     .replace(/--+/g, '-') // Replace multiple - with single -
     .replace(/^-+/, '') // Trim - from start of text
     .replace(/-+$/, ''); // Trim - from end of text
+
 const unique = (array) =>
   array.filter((value, index, self) => self.indexOf(value) === index);
 
