0d7b829e39a65129c14f9ef1baf5850d42ec5cfb
Jonny Gerig Meyer <jonny@oddbird.net>
2020-07-07 15:43:52 -0400

    Fix oss sorting

--- .eleventy.js
+++ .eleventy.js
@@ -54,7 +54,7 @@ module.exports = (eleventyConfig) => {
     collection
       .getAll()
       .filter((item) => item.data.oss)
-      .sort((a, b) => b.sort - a.sort),
+      .sort((a, b) => b.date - a.date),
   );
   eleventyConfig.addCollection('work', (collection) =>
     collection
