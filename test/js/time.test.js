77da471c60f4d02e131da41aeeb70b0d1b683852
Jonny Gerig Meyer <jonny@oddbird.net>
2021-05-05 17:40:26 -0400

    Replace custom date formatting with date-fns

--- test/js/time.test.js
+++ test/js/time.test.js
@@ -3,8 +3,8 @@ const { getDate } = require('#/time');
 describe('time filters', () => {
   test('getDate', () => {
     const date = '2020-02-01';
-    const expected = '02/01/2020';
 
-    expect(getDate(date, 'slash')).toEqual(expected);
+    expect(getDate(date, 'url')).toEqual('2020/02/01');
+    expect(getDate(date, 'eeee')).toEqual('Saturday');
   });
 });
