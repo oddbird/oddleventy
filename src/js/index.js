a9fa83037346a8cb7dba856e1046de44af56cd96
Jonny Gerig Meyer <jonny@oddbird.net>
2022-04-15 14:53:14 -0400

    crawl search-interface branch

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ algoliasearchNetlify({
   appId: 'WVUJ3O7KS6',
   apiKey: 'd71de03d6d2a856252f54ea24bdce576',
   siteId: '4f75b5a7-8412-4586-bad0-b4de64bb4f17',
-  branch: 'main',
+  branch: 'search-interface',
   selector: '#search',
 });
 
