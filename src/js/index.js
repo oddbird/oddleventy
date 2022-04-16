dfa2f807d9439bd880d5bd2c5934568b902dc504
Jonny Gerig Meyer <jonny@oddbird.net>
2022-04-16 11:16:21 -0400

    Show both searches; 10 results

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,19 @@ algoliasearchNetlify({
   appId: 'WVUJ3O7KS6',
   apiKey: 'd71de03d6d2a856252f54ea24bdce576',
   siteId: '4f75b5a7-8412-4586-bad0-b4de64bb4f17',
-  // @@@ update this before merge
-  branch: 'search-interface',
+  branch: 'main',
   selector: '#search',
+  hitsPerPage: 10,
+});
+
+// @@@ update this before merge
+algoliasearchNetlify({
+  appId: 'WVUJ3O7KS6',
+  apiKey: 'd71de03d6d2a856252f54ea24bdce576',
+  siteId: '4f75b5a7-8412-4586-bad0-b4de64bb4f17',
+  branch: 'search-interface',
+  selector: '#search-hierarchical',
+  hitsPerPage: 10,
 });
 
 // Initialize Cascading Colors
