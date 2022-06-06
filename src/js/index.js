2e733930958c346fe7d216a007891074f5496757
Stacy Kvernmo <stacy@oddbird.net>
2022-06-06 16:52:57 -0500

    additional style clean up, remove heirarchical search form

--- src/js/index.js
+++ src/js/index.js
@@ -12,16 +12,6 @@ algoliasearchNetlify({
   hitsPerPage: 10,
 });
 
-// @@@ update this before merge
-algoliasearchNetlify({
-  appId: 'WVUJ3O7KS6',
-  apiKey: 'd71de03d6d2a856252f54ea24bdce576',
-  siteId: '4f75b5a7-8412-4586-bad0-b4de64bb4f17',
-  branch: 'search-interface',
-  selector: '#search-hierarchical',
-  hitsPerPage: 10,
-});
-
 // Initialize Cascading Colors
 // https://www.oddbird.net/cascading-colors/
 ccs();
