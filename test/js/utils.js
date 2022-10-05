935c54709c2528fead81913e4f2ffe5dc14a24be
Jonny Gerig Meyer <jonny@oddbird.net>
2022-10-05 17:17:27 -0400

    test

--- test/js/utils.js
+++ test/js/utils.js
@@ -274,6 +274,81 @@ export const collection4 = [
     templateContent: '<h1>This is my title</h1>\n\n<p>This is content…',
   },
 ];
+
 export const collections = {
   all: [collection, collection2, collection3, collection4],
+};
+
+export const webmentions = {
+  lastFetched: '2022-10-05T18:13:51.667Z',
+  children: [
+    {
+      type: 'entry',
+      author: {
+        type: 'card',
+        name: 'Reviewer 1',
+      },
+      url: 'https://example.com',
+      published: '2022-08-18T18:23:32+00:00',
+      'wm-received': '2022-08-19T14:59:23Z',
+      'wm-id': 1,
+      'wm-target': 'https://www.oddbird.net/1/',
+      content: {
+        html: 'so excited for this!',
+      },
+      'wm-property': 'in-reply-to',
+      'wm-private': false,
+    },
+    {
+      type: 'entry',
+      author: {
+        type: 'card',
+        name: 'Reviewer 2',
+      },
+      url: 'https://example.com',
+      published: '2022-08-18T17:40:38+00:00',
+      'wm-received': '2022-08-19T14:59:20Z',
+      'wm-id': 2,
+      'wm-target': 'https://www.oddbird.net/1?ref=foo',
+      content: {
+        html: '🥳 Size Container Queries',
+      },
+      'wm-property': 'mention-of',
+      'wm-private': false,
+    },
+    {
+      type: 'entry',
+      author: {
+        type: 'card',
+        name: 'Reviewer 3',
+      },
+      url: 'https://example.com',
+      published: '2022-08-18T17:40:38+00:00',
+      'wm-received': '2022-08-19T14:59:20Z',
+      'wm-id': 3,
+      'wm-target': 'https://www.oddbird.net/1/',
+      content: {
+        html: '🥳 Size Container Queries',
+      },
+      'wm-property': 'repost-of',
+      'wm-private': false,
+    },
+    {
+      type: 'entry',
+      author: {
+        type: 'card',
+        name: 'Reviewer 4',
+      },
+      url: 'https://example.com',
+      published: '2022-08-18T18:40:44+00:00',
+      'wm-received': '2022-08-19T14:59:20Z',
+      'wm-id': 4,
+      'wm-target': 'https://www.oddbird.net/2/',
+      content: {
+        html: 'Completely understand',
+      },
+      'wm-property': 'in-reply-to',
+      'wm-private': false,
+    },
+  ],
 };
