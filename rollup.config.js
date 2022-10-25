b668a4d924be89ef0cffd2900ffb371bf4d6a7d3
Jonny Gerig Meyer <jonny@oddbird.net>
2022-10-25 14:38:04 -0400

    Remove Yarn PnP

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-minification';
 
 export default {
   input: 'src/js/index.js',
   output: {
     file: '_built/js/scripts.js',
     format: 'iife',
-    sourcemap: true,
+    sourcemap: false,
   },
   plugins: [
     nodeResolve({ browser: true }),
--- scripts/compile-sass.cjs
+++ scripts/compile-sass.cjs
@@ -3,6 +3,7 @@
 'use strict';
 
 const path = require('path');
+const { pathToFileURL } = require('url');
 
 const autoprefixer = require('autoprefixer');
 const chalk = require('chalk');
@@ -26,42 +27,43 @@ try {
 
 const resolved = new Map();
 
-const nodeImporter = (url, prev, done) => {
-  if (url.startsWith('~')) {
-    if (pnp) {
-      // We need to resolve the imported path to a node module path. Instead of
-      // re-creating the Sass import rules (where, for example,
-      // ``@import 'foo'`` can be shorthand for ``@import 'foo/_index.scss'``),
-      // we extract the module name from the full path, resolve that, then
-      // re-add the rest of the path.
-      const components = url.substring(1).split('/');
-      // Adjust for scoped packages (e.g. ``@foo/bar``)...
-      if (components[0].startsWith('@') && components.length > 1) {
-        components[0] = components.slice(0, 2).join('/');
-        components.splice(1, 1);
-      }
-      let res;
-      if (resolved.has(components[0])) {
-        res = resolved.get(components[0]);
-      } else {
-        res = pnp.resolveToUnqualified(components[0], `${baseDir}/`);
-        resolved.set(components[0], res);
-      }
-      if (res === null) {
-        // No resolved module found... fall back to default Sass importer
-        done(null);
-      } else {
+const nodeImporter = {
+  findFileUrl(url) {
+    if (url.startsWith('~')) {
+      if (pnp) {
+        // We need to resolve the imported path to a node module path. Instead of
+        // re-creating the Sass import rules (where, for example,
+        // ``@import 'foo'`` can be shorthand for ``@import 'foo/_index.scss'``),
+        // we extract the module name from the full path, resolve that, then
+        // re-add the rest of the path.
+        const components = url.substring(1).split('/');
+        // Adjust for scoped packages (e.g. ``@foo/bar``)...
+        if (components[0].startsWith('@') && components.length > 1) {
+          components[0] = components.slice(0, 2).join('/');
+          components.splice(1, 1);
+        }
+        let res;
+        if (resolved.has(components[0])) {
+          res = resolved.get(components[0]);
+        } else {
+          res = pnp.resolveToUnqualified(components[0], `${baseDir}/`);
+          resolved.set(components[0], res);
+        }
+        if (res === null) {
+          // No resolved module found... fall back to default Sass importer
+          return null;
+        }
         components[0] = res;
-        done({ file: components.join('/') });
+        return new URL(pathToFileURL(components.join('/')));
       }
-    } else {
       // Try loading file from "node_modules" dir
-      done({ file: path.join(baseDir, 'node_modules', url.substring(1)) });
+      return new URL(
+        pathToFileURL(path.join(baseDir, 'node_modules', url.substring(1))),
+      );
     }
-  } else {
     // Fall back to default Sass importer
-    done(null);
-  }
+    return null;
+  },
 };
 
 const outputFile = (file, inputName, contents) =>
@@ -72,59 +74,53 @@ const outputFile = (file, inputName, contents) =>
     }
   });
 
-const compileSass = ({ name, sourceMap, postCSS }) => {
+const compileSass = ({ name, usePostCSS }) => {
   const inFilename = `${name}.scss`;
   const inFile = path.join(inDir, inFilename);
   const outFilename = `${name}.css`;
   const outFile = path.join(outDir, outFilename);
   const outMap = path.join(outDir, `${outFilename}.map`);
   const destCssFile = path.join(destCssDir, outFilename);
-  sass.render(
-    {
-      file: inFile,
-      outputStyle: 'compressed',
-      sourceMap: Boolean(sourceMap),
-      sourceMapContents: true,
-      outFile: outFilename,
-      importer: nodeImporter,
-    },
-    (compileErr, result) => {
-      if (compileErr) {
-        throw compileErr.formatted;
-      }
-      if (postCSS) {
-        return postcss([autoprefixer])
-          .process(result.css, {
-            from: undefined,
-            to: destCssFile,
-            map: {
-              prev: result.map.toString(),
-            },
-          })
-          .then((res) => {
-            res.warnings().forEach((warn) => {
-              console.warn(chalk.yellow(warn.toString()));
-            });
-            if (sourceMap && res.map) {
-              outputFile(outMap, inFilename, res.map);
-            }
-            return outputFile(outFile, inFilename, res.css);
-          })
-          .catch((err) => {
-            console.error(chalk.red(err));
+  try {
+    const result = sass.compile(inFile, {
+      style: 'compressed',
+      sourceMap: usePostCSS,
+      sourceMapIncludeSources: true,
+      importers: [nodeImporter],
+    });
+    if (usePostCSS) {
+      return postcss([autoprefixer])
+        .process(result.css, {
+          from: inFile,
+          to: destCssFile,
+          map: {
+            annotation: true,
+            prev: result.sourceMap,
+            sourcesContent: true,
+          },
+        })
+        .then((res) => {
+          res.warnings().forEach((warn) => {
+            console.warn(chalk.yellow(warn.toString()));
           });
-      }
-      if (sourceMap && result.map) {
-        outputFile(outMap, inFilename, result.map);
-      }
-      return outputFile(outFile, inFilename, result.css);
-    },
-  );
+          if (res.map) {
+            outputFile(outMap, inFilename, res.map);
+          }
+          return outputFile(outFile, inFilename, res.css);
+        })
+        .catch((err) => {
+          console.error(chalk.red(err));
+        });
+    }
+    return outputFile(outFile, inFilename, result.css);
+  } catch (error) {
+    throw error.message;
+  }
 };
 
-compileSass({ name: 'screen', sourceMap: true, postCSS: true });
-compileSass({ name: 'styleguide', sourceMap: true, postCSS: true });
+compileSass({ name: 'screen', usePostCSS: true });
+compileSass({ name: 'styleguide', usePostCSS: true });
 compileSass({ name: 'json' });
 
 // @@@ page-specific CSS; this should maybe not be hardcoded?
-compileSass({ name: 'page/support-unknown', sourceMap: true, postCSS: true });
+compileSass({ name: 'page/support-unknown', usePostCSS: true });
