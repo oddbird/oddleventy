1ab4336d3dc37d1e6d1c4225cb5c78492a69b5ea
James Stuckey Weber <james@oddbird.net>
2024-06-14 13:57:33 +0000

    Adjust image sizes

--- src/filters/support.js
+++ src/filters/support.js
@@ -14,18 +14,26 @@ params:
   username:
     type: string
     default: ''
+  size:
+    type: string
+    default: 'sm'
 */
 export const openCollectiveAvatar = (url, username = '', size = 'sm') => {
   const sizeNumber = {
-    sm: '66',
-    md: '88',
-    lg: '110',
+    sm: '80',
+    md: '120',
+    lg: '160',
   }[size];
   const alt = `Open Collective Avatar${username ? ` for ${username}` : ''}`;
   const sizeString = `width="${sizeNumber}" height="${sizeNumber}"`;
+  let urlWithSize = url;
+
+  if (urlWithSize.includes('gravatar.com')) {
+    urlWithSize = `${urlWithSize}&s=${sizeNumber}`;
+  }
 
   return `<img src="https://v1.image.11ty.dev/${encodeURIComponent(
-    url,
+    urlWithSize,
     // eslint-disable-next-line max-len
   )}/webp/${sizeNumber}/" ${sizeString} alt="${alt}" loading="lazy" decoding="async">`;
 };
--- test/js/support.test.js
+++ test/js/support.test.js
@@ -6,20 +6,23 @@ describe('support filters', () => {
   test('openCollectiveAvatar', () => {
     const url = 'https://v1.image.11ty.dev/https%3A%2F%2Fwww.example.com/webp/';
     expect(openCollectiveAvatar('https://www.example.com', 'User Name')).toBe(
-      `<img src="${url}66/" width="66" height="66" alt="Open Collective Avatar for User Name" loading="lazy" decoding="async">`,
+      `<img src="${url}80/" width="80" height="80" alt="Open Collective Avatar for User Name" loading="lazy" decoding="async">`,
     );
     expect(openCollectiveAvatar('https://www.example.com')).toBe(
-      `<img src="${url}66/" width="66" height="66" alt="Open Collective Avatar" loading="lazy" decoding="async">`,
+      `<img src="${url}80/" width="80" height="80" alt="Open Collective Avatar" loading="lazy" decoding="async">`,
+    );
+    expect(openCollectiveAvatar('https://www.gravatar.com')).toBe(
+      `<img src="https://v1.image.11ty.dev/https%3A%2F%2Fwww.gravatar.com%26s%3D80/webp/80/" width="80" height="80" alt="Open Collective Avatar" loading="lazy" decoding="async">`,
     );
     expect(
       openCollectiveAvatar('https://www.example.com', 'User Name', 'md'),
     ).toBe(
-      `<img src="${url}88/" width="88" height="88" alt="Open Collective Avatar for User Name" loading="lazy" decoding="async">`,
+      `<img src="${url}120/" width="120" height="120" alt="Open Collective Avatar for User Name" loading="lazy" decoding="async">`,
     );
     expect(
       openCollectiveAvatar('https://www.example.com', 'User Name', 'lg'),
     ).toBe(
-      `<img src="${url}110/" width="110" height="110" alt="Open Collective Avatar for User Name" loading="lazy" decoding="async">`,
+      `<img src="${url}160/" width="160" height="160" alt="Open Collective Avatar for User Name" loading="lazy" decoding="async">`,
     );
   });
 });
