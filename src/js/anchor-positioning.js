c1ea1c7713b03d325b12bfae1da1937c38ce42df
James Stuckey Weber <james@oddbird.net>
2025-07-08 16:19:58 -0400

    Add dropdown menu pattern

---
+++ src/js/anchor-positioning.js
@@ -0,0 +1,14 @@
+export default async function checkPolyfill() {
+
+
+
+  if (!("anchorName" in document.documentElement.style)) {
+    const { default: polyfill } = await import("https://unpkg.com/@oddbird/css-anchor-positioning/dist/css-anchor-positioning-fn.js");
+    polyfill({
+      elements: [...document.querySelectorAll('link[rel="stylesheet"]')],
+      excludeInlineStyles: true,
+    });
+  }
+
+
+}
