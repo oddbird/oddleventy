ab86a01d110b37e0dc3f8c10c0c62ab1d4428f27
James Stuckey Weber <james@oddbird.net>
2025-07-09 11:03:38 -0400

    Adjustments

--- src/js/anchor-positioning.js
+++ src/js/anchor-positioning.js
@@ -1,14 +1,11 @@
 export default async function checkPolyfill() {
-
-
-
-  if (!("anchorName" in document.documentElement.style)) {
-    const { default: polyfill } = await import("https://unpkg.com/@oddbird/css-anchor-positioning/dist/css-anchor-positioning-fn.js");
+  if (!('anchorName' in document.documentElement.style)) {
+    const { default: polyfill } = await import(
+      'https://unpkg.com/@oddbird/css-anchor-positioning/dist/css-anchor-positioning-fn.js'
+    );
     polyfill({
       elements: [...document.querySelectorAll('link[rel="stylesheet"]')],
       excludeInlineStyles: true,
     });
   }
-
-
 }
