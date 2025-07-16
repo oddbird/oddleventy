1d7e40a601a4c2630f61b3fe9247313d2b2514e2
James Stuckey Weber <james@oddbird.net>
2025-07-16 12:03:20 -0400

    Switch to ESM imports

--- src/js/anchor-positioning.js
+++ src/js/anchor-positioning.js
@@ -1,7 +1,7 @@
 export default async function checkPolyfill() {
   if (!('anchorName' in document.documentElement.style)) {
     const { default: polyfill } = await import(
-      'https://unpkg.com/@oddbird/css-anchor-positioning/dist/css-anchor-positioning-fn.js'
+      '@oddbird/css-anchor-positioning/fn'
     );
     polyfill({
       elements: [...document.querySelectorAll('link[rel="stylesheet"]')],
